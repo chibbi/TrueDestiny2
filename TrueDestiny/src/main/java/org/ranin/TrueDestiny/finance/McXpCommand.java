0fdc38204066b29e8c7596390b0557e14a8a61a8
chibbi <chehab.djibril@gmail.com>
2021-02-02 23:00:44 +0100

    fixed xp cmd

--- TrueDestiny/src/main/java/org/ranin/TrueDestiny/finance/McXpCommand.java
+++ TrueDestiny/src/main/java/org/ranin/TrueDestiny/finance/McXpCommand.java
@@ -17,11 +17,11 @@ public class McXpCommand implements CommandExecutor, TabCompleter {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 2) {
-                player.sendMessage("§6STUFF §7" + args[1] + "  §7" + args[2] + " §6xp");
+                player.sendMessage("§6STUFF §7" + args[0] + "  §7" + args[1] + " §6xp");
                 if (player.getExpToLevel() - Integer.valueOf(args[1]) >= 0) {
-                    player.setLevel(player.getExpToLevel() - Integer.valueOf(args[1]));
-                    Bukkit.getPlayer(args[1]).setLevel(player.getExpToLevel() + Integer.valueOf(args[1]));
-                    player.sendMessage("§6gave §7" + args[1] + "  §7" + args[2] + " §6xp");
+                    player.setLevel(player.getExpToLevel() - Integer.valueOf(args[0]));
+                    Bukkit.getPlayer(args[0]).setLevel(player.getExpToLevel() + Integer.valueOf(args[1]));
+                    player.sendMessage("§6gave §7" + args[0] + "  §7" + args[1] + " §6xp");
                     return true;
                 } else {
                     player.sendMessage("§6you don't have enough xp");
