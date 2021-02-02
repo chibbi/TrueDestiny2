ca019f40dd8f961a271d10b5b518208d28fe08a4
chibbi <chehab.djibril@gmail.com>
2021-02-02 23:12:27 +0100

    fixed xpcommand 2

--- TrueDestiny/src/main/java/org/ranin/TrueDestiny/finance/McXpCommand.java
+++ TrueDestiny/src/main/java/org/ranin/TrueDestiny/finance/McXpCommand.java
@@ -18,7 +18,7 @@ public class McXpCommand implements CommandExecutor, TabCompleter {
             Player player = (Player) sender;
             if (args.length == 2) {
                 if (player.getLevel() - Integer.valueOf(args[1]) >= 0) {
-                    player.giveExpLevels(Integer.valueOf(args[1]));
+                    player.giveExpLevels(-Integer.valueOf(args[1]));
                     Bukkit.getPlayer(args[0]).giveExpLevels(Integer.valueOf(args[1]));
                     player.sendMessage("§6gave §7" + args[0] + "  §7" + args[1] + " §6xp");
                     return true;
