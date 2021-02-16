07f9e6f8e45ba802cf29cd5a01adc2ebbe9e4d6b
chibbi <chehab.djibril@gmail.com>
2021-02-16 18:22:33 +0100

    removed 'DISALLOWED TOOL, ...' message

--- TrueDestiny/src/main/java/org/ranin/TrueDestiny/job/classes/Job.java
+++ TrueDestiny/src/main/java/org/ranin/TrueDestiny/job/classes/Job.java
@@ -57,7 +57,6 @@ abstract class Job {
     // #endregion
 
     protected final EnumSet<Material> allMaterials = EnumSet.allOf(Material.class);
-    protected final EnumSet<Material> redstone;
     protected final EnumSet<Material> stoneBlocks;
     protected final EnumSet<Material> flowers;
     protected final EnumSet<Material> common;
@@ -169,10 +168,6 @@ abstract class Job {
                 Material.POLISHED_BLACKSTONE_BRICK_WALL };
         stoneBlocks = createEnum(temporary);
         flowers = allMaterials.range(Material.DANDELION, Material.WITHER_ROSE); // THAT WORKS
-        temporary = new Material[] { Material.PISTON, Material.STICKY_PISTON, Material.REPEATER, Material.COMPARATOR,
-                Material.REDSTONE, Material.REDSTONE_LAMP, Material.REDSTONE_TORCH, Material.DROPPER, Material.LEVER,
-                Material.DISPENSER };
-        redstone = createEnum(temporary);
         temporary = new Material[] { Material.WHITE_BED, Material.YELLOW_BED, Material.RED_BED, Material.PURPLE_BED,
                 Material.PINK_BED, Material.ORANGE_BED, Material.MAGENTA_BED, Material.LIME_BED,
                 Material.LIGHT_GRAY_BED, Material.LIGHT_BLUE_BED, Material.GREEN_BED, Material.GRAY_BED,
@@ -370,7 +365,6 @@ abstract class Job {
                     }
                 }
             }
-            event.getPlayer().sendMessage("DISALLOWED TOOL, " + event.getMaterial());
             return onBreaking(event);
         }
 
